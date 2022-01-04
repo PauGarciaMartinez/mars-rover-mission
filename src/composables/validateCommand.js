98eab641ea10487ea4034013ccf44a53e8ff5e64
Pau Garcia Martínez <etpau@hotmail.es>
2022-01-04 10:51:04 +0100

    Fix position major than 20 not triggering error

--- src/composables/validateCommand.js
+++ src/composables/validateCommand.js
@@ -1,7 +1,7 @@
 const validateCommand = (position) => {
   let isValid = true;
 
-  if (position.x < 0 || position.y < 0) {
+  if (position.x < 0 || position.y < 0 || position.x > 20 || position.y > 20) {
     isValid = false;
     alert("Well, it seems Mars is flat after all...");
   }
