9ca9e7bf1e19cca5f6e77a0358f53c611e7c2683
Pau Garcia Martínez <etpau@hotmail.es>
2022-01-04 20:13:39 +0100

    Start alert component

--- src/components/AlertMessage.vue
+++ src/components/AlertMessage.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Alert!
+  <div class="alert-container">
+    <p class="alert-message">
+      <span>Rover:</span>
+    </p>
   </div>
 </template>
 
@@ -11,5 +13,18 @@ export default {
 </script>
 
 <style>
-
+.alert-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.alert-message {
+  color: white;
+  background-color: #2c3e50;
+  padding: 0.6rem 2rem;
+  border-radius: 0.2rem;
+}
+.alert-message span {
+  font-weight: 600;
+}
 </style>
